b08c94ca3b4e60854dc06f64d4904d22b953b12c
Farzan <farzan.hosseiny@gmail.com>
2023-05-16 21:43:33 +0330

    optional chaining compeleted

--- Intersection-type-question/src/index.ts
+++ Intersection-type-question/src/index.ts
@@ -1,18 +1,22 @@
-
 type Teacher = {
-  //implement teacher types
+  teacher_id: number;
+  teacher_name: string;
+  teacher_age: string;
 };
 
 type Student = {
-  //implement student types
+  student_id: number;
+  student_name: string;
+  student_grade: string;
 };
 
- type intersected_type = /* implement intersected_type type */;
+type intersected_type = Teacher & Student;
 
 const University: intersected_type = {
+  name: "mr.bronze",
+  field: "computer sience",
+  class: "101",
+  department: "computer",
 };
 
-
-
-
-export {  intersected_type };
+export { intersected_type };
